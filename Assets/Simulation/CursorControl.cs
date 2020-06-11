6d9769f30d2d295c69d368ada29526ca1c3f76fe
saiyam-jain <saiyam_j@yahoo.com>
2020-06-11 18:10:27 +0530

    No much code changes, added ProjectDesignDocument

--- Assets/Simulation/CursorControl.cs
+++ Assets/Simulation/CursorControl.cs
@@ -12,6 +12,24 @@ public class CursorControl : MonoBehaviour
 
     private void Update() //Runs when mouse is hovering on the simulation.
     {
+        UpdateCursor();
+        //UpdatePauseResume();
+    }
+
+    // I thought of adding pause, resume and reduce the play speed of simulation, 
+    // but I realised that the below commands will pause the game, but not the shader computation
+    //private void UpdatePauseResume()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.P))
+    //        Time.timeScale = 0.0f;
+    //    else if (Input.GetKeyDown(KeyCode.R))
+    //        Time.timeScale = 1.0f;
+    //    else if (Input.GetKeyDown(KeyCode.Space))
+    //        Time.timeScale = 0.5f;
+    //}
+
+    private void UpdateCursor()
+    {
         RaycastHit hit;
         Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -26,18 +44,14 @@ public class CursorControl : MonoBehaviour
 
         if (!Physics.Raycast(rayOrigin, out hit))
         {
+            Debug.Log("Mouse is not on Simulation");
+            simulation.Shader.SetFloat("mouseradius", 0);
             return;
         }
 
+        Debug.Log("Mouse on simulation");
         simulation.Shader.SetFloat("mouseRadius", mouseRadius);
         mouseUV = hit.textureCoord;     //Passing mouse Tex Coord to GPU
         simulation.Shader.SetVector("mouseUV", mouseUV);
     }
-    private void OnMouseExit()
-    {
-        Debug.Log("Mouse is not on Simulation");
-        simulation.Shader.SetFloat("mouseradius", 0);
-        return;
-    }
-
 }
